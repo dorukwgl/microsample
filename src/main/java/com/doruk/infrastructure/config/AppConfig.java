fcf0dfafd25acd1c5341e2dedd1fd86008e2ae2a
dorukwagle <dorukwagle@gmail.com>
2025-12-05 16:16:41 +0545

    add upload paths in config

--- src/main/java/com/doruk/infrastructure/config/AppConfig.java
+++ src/main/java/com/doruk/infrastructure/config/AppConfig.java
@@ -4,5 +4,5 @@ package com.doruk.infrastructure.config;
 import io.micronaut.context.annotation.ConfigurationProperties;
 
 @ConfigurationProperties("micronaut.application")
-public record AppConfig(String name) {
+public record AppConfig(String name, String publicUploadPath, String privateUploadPath) {
 }
