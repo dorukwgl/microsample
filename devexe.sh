c94e02a720905245b6859e0dfa01cb315881cebc
dorukwagle <dorukwagle@gmail.com>
2025-12-07 15:39:11 +0545

    minor change

--- devexe.sh
+++ devexe.sh
@@ -4,5 +4,6 @@ export DATASOURCE_USERNAME=doruk
 export MICROSAMPLE_PORT=9096
 export DATASOURCE_URL="postgresql://localhost:5432/microsample"
 export DATASOURCE_PASSWORD=dorukdb
+export LOG_DIR="/var/log/microsample"
 
 ./mvnw mn:run -Pdocs
