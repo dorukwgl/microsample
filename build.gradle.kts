9c8af29a8b6a66e3950dfbef94d899cd5801f026
dorukwagle <dorukwagle@gmail.com>
2026-01-27 20:17:58 +0545

    add epoll

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     runtimeOnly("org.yaml:snakeyaml")
     // Native transport
     runtimeOnly("io.netty:netty-transport-native-io_uring::linux-x86_64")
-//    runtimeOnly("io.netty:netty-transport-native-epoll::linux-x86_64")
+    runtimeOnly("io.netty:netty-transport-native-epoll::linux-x86_64")
 
     developmentOnly("io.micronaut.controlpanel:micronaut-control-panel-management")
     developmentOnly("io.micronaut.controlpanel:micronaut-control-panel-ui")
