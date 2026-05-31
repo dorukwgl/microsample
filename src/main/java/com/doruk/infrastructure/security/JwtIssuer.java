f6905f6faa6ef6b7e4a0b54dd25e6b8dbf4273bc
dorukwagle <dorukwagle@gmail.com>
2026-05-31 17:53:15 +0545

    fix jwks error handling

--- src/main/java/com/doruk/infrastructure/security/JwtIssuer.java
+++ src/main/java/com/doruk/infrastructure/security/JwtIssuer.java
@@ -1,10 +1,12 @@
 package com.doruk.infrastructure.security;
 
 import com.doruk.application.app.auth.dto.JwtResponse;
+import com.doruk.application.exception.ApplicationException;
 import com.doruk.infrastructure.apiclient.JwksClient;
 import com.doruk.infrastructure.security.mappers.JwksRequestMapper;
 import com.doruk.infrastructure.security.mappers.JwksTokenMapper;
 import com.doruk.application.app.auth.dto.JwtRequest;
+import io.micronaut.http.client.exceptions.HttpClientException;
 import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 
@@ -16,8 +18,12 @@ public class JwtIssuer {
     private final JwksRequestMapper jwksRequestMapper;
 
     public JwtResponse issueToken(JwtRequest request) {
-        return jwksTokenMapper.toTokenResponse(
-                jwksClient.issueToken(jwksRequestMapper.toJwksRequest(request))
-        );
+        try {
+            return jwksTokenMapper.toTokenResponse(
+                    jwksClient.issueToken(jwksRequestMapper.toJwksRequest(request))
+            );
+        } catch (HttpClientException e) {
+            throw new RuntimeException("Authentication service is currently unavailable. Please try again later.", e);
+        }
     }
 }
